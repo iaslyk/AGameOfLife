119bc6390794243f620e6a60c079b5e0f568d50b
Oliver <oliversulyok@gmail.com>
2021-01-05 19:30:07 +0100

    agameoflife.py
    Added import functions and call main

---
+++ agameoflife.py
@@ -0,0 +1,13 @@
+# Conway's Game Of Life
+# Oliver Sulyok
+
+
+import argparse
+import numpy as np
+import matplotlib.pyplot as plt 
+import matplotlib.animation as animation
+
+
+# call main
+if __name__ == '__main__':
+    main()
